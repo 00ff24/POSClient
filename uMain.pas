199531be0d2f39d85905d3d72d0ab0dc636f6250
Cristian Garcia <criistiann24+github@gmail.com>
2024-05-07 09:53:27 -0400

    se quito el messageDlg de cerrar sesion

--- uMain.pas
+++ uMain.pas
@@ -5,16 +5,16 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
-  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts, uClient;
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts, uClient, FMX.DialogService;
 
 type
   TmainFrm = class(TForm)
-    Button1: TButton;
-    Button5: TButton;
-    Button9: TButton;
+    ventasBtn: TButton;
+    facturasBtn: TButton;
+    clientesBtn: TButton;
     Layout1: TLayout;
     procedure FormClose(Sender: TObject; var Action: TCloseAction);
-    procedure Button1Click(Sender: TObject);
+    procedure ventasBtnClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -29,14 +29,23 @@ implementation
 {$R *.fmx}
 
 //C.G. Cuando cierro el formulario principal se detiene la aplicacion. (le doy hacia atras estando en el formulario main)
-procedure TmainFrm.Button1Click(Sender: TObject);
+procedure TmainFrm.ventasBtnClick(Sender: TObject);
 begin
 clientFrm.Show;
 end;
 
 procedure TmainFrm.FormClose(Sender: TObject; var Action: TCloseAction);
 begin
-Application.Terminate;
+  TDialogService.MessageDialog('¿Quieres cerrar sesión?', TMsgDlgType.mtConfirmation,
+    [TMsgDlgBtn.mbYes, TMsgDlgBtn.mbNo], TMsgDlgBtn.mbNo, 0,
+    procedure(const AResult: TModalResult)
+    begin
+      case AResult of
+        mrYes: Application.Terminate;
+      end;
+    end);
 end;
+
+
 
 end.
